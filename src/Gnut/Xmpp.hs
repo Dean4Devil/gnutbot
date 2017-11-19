6539b6208b34737b469c2ca0934470b4eb63b0a5
Gregor Reitzenstein <dean4devil@paranoidlabs.org>
2017-11-19 16:33:51 +0100

    Make Gnut.Xmpp mainly re-export non-clashing names

--- src/Gnut/Xmpp.hs
+++ src/Gnut/Xmpp.hs
@@ -1,12 +1,19 @@
 module Gnut.Xmpp
-    ( setupSession
-    , getMessage
-    , xmppLoop
+    ( Message(..)
+    , Session(..)
+    , Jid(..)
+    , Presence(..)
+    , PresenceType(..)
+    , sendPresence
+    , parseJid
+
+    , InstantMessage(..)
+    , MessageBody(..)
+    , getIM
     )
     where
 
-import Gnut.Config
-import qualified Gnut.Types as G
+import Gnut.Config (Config)
 
 import Control.Lens
 import Control.Monad
@@ -16,29 +23,29 @@ import Data.Default
 
 import qualified Data.Text as T
 
-import Reactive.Banana.Frameworks
-
 import Network.Xmpp
-import Network.Xmpp
+import Network.Xmpp.IM
 import Network.Xmpp.Internal (TlsBehaviour(..))
 
-setupSession :: Config -> IO Session
-setupSession c = do
-    result <- session (T.unpack $ c^.connection.domain) (Just (const [plain (c^.connection.user) Nothing (c^.connection.password)], Nothing)) $ def
-        & streamConfigurationL .  tlsBehaviourL .~ RequireTls
-        & onConnectionClosedL .~ reconnectSession
-    sess <- case result of
-            Right s -> return s
-            Left e -> liftIO $ error $ "XMPP Failure: " ++ show e
-    sendPresence presenceOnline sess
-    return sess
-  where
-    reconnectSession sess failure = void (reconnect' sess)
-
-xmppLoop :: Session -> Handler Message -> IO ()
-xmppLoop sess sink = loop
-    where
-    loop = do
-        m <- getMessage sess
-        _ <- sink m
-        loop
+{-
+ -setupSession :: Config -> IO Session
+ -setupSession c = do
+ -    result <- session (T.unpack $ c^.connection.domain) (Just (const [plain (c^.connection.user) Nothing (c^.connection.password)], Nothing)) $ def
+ -        & streamConfigurationL .  tlsBehaviourL .~ RequireTls
+ -        & onConnectionClosedL .~ reconnectSession
+ -    sess <- case result of
+ -            Right s -> return s
+ -            Left e -> liftIO $ error $ "XMPP Failure: " ++ show e
+ -    sendPresence presenceOnline sess
+ -    return sess
+ -  where
+ -    reconnectSession sess failure = void (reconnect' sess)
+ -
+ -xmppLoop :: Session -> Handler Message -> IO ()
+ -xmppLoop sess sink = loop
+ -    where
+ -    loop = do
+ -        m <- getMessage sess
+ -        _ <- sink m
+ -        loop
+ -}
